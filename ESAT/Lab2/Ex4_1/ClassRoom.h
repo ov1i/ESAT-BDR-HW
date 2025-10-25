a59ca24d05ff5770feaa74a8614e60fb3f03305a
Ovidiu Gherman <govidiu15@gmail.com>
2025-10-26 01:28:36 +0300

    ex4_1

--- ESAT/Lab2/Ex4_1/ClassRoom.h
+++ ESAT/Lab2/Ex4_1/ClassRoom.h
@@ -3,9 +3,8 @@
 #include <cstring>
 
 namespace cls {
-
-    constexpr uint8_t NO_OF_MARKS = 7;
     constexpr uint8_t CHAR_ARR_MAX_LEN = 255;
+
     class C_Student {
     public:
         /// @brief          Explicit Constrcutor
@@ -13,7 +12,7 @@ namespace cls {
         /// @param surname  -> Student surname
         /// @param marks    -> Student marks (7)
         /// @param group    -> Student group
-        C_Student(char *name, char *surname, uint16_t *marks, uint16_t group);
+        C_Student(char *name, char *surname, uint16_t *marks, uint16_t marksCount);
 
         /// @brief          Explicit destructor
         ~C_Student();
@@ -34,14 +33,14 @@ namespace cls {
         /// @return         Pointer to the first element of the array
         uint16_t *getMarks();
 
-        /// @brief          Getter Group
-        /// @return         m_group
-        uint16_t getGroup();
+        /// @brief          Getter Number of Marks
+        /// @return         m_markCount
+        uint16_t getMarksCount();
     private:
         char        *m_name;
         char        *m_surname;
-        uint16_t     m_marks[NO_OF_MARKS];
-        uint16_t    m_group;
+        uint16_t    *m_marks;
+        uint16_t    m_marksCount;
     };
     
     typedef struct {
@@ -49,25 +48,31 @@ namespace cls {
         double avg_mark;
     } StudentSituation_t;
 
-    C_Student::C_Student(char *name, char *surname, uint16_t *marks, uint16_t group) : m_group(group) {
+    C_Student::C_Student(char *name, char *surname, uint16_t *marks, uint16_t marksCount) : m_marksCount(marksCount) {
         size_t lenName = strlen(name);
         size_t lenSurname = strlen(surname);
 
-        m_name = (char*)malloc(lenName * sizeof(char));
+        m_name = (char*)malloc(sizeof(char) * lenName);
         if(!m_name) {
             cout << "Failed to alloc memory for the name\n";
             return;
         }
         
-        m_surname = (char*)malloc(lenSurname * sizeof(char));
+        m_surname = (char*)malloc(sizeof(char) * lenSurname);
         if(!m_surname) {
             cout << "Failed to alloc memory for the surname\n";
+            return;
+        }
+
+        m_marks = (uint16_t*)malloc(sizeof(uint16_t) * m_marksCount);
+        if(!m_marks) {
+            cout << "Failed to alloc memory for the marks\n";
             return;
         }
         
         strcpy(m_surname, surname);
         strcpy(m_name, name);
-        memcpy(&m_marks, marks, sizeof(uint16_t) * 7);
+        memcpy(m_marks, marks, sizeof(uint16_t) * m_marksCount);
     }
     
     C_Student::~C_Student() {
@@ -76,17 +81,17 @@ namespace cls {
 
     double C_Student::computeAvg() {
         uint16_t sum = 0U;
-        for(uint8_t i = 0U; i < 7U; i++) {
+        for(uint16_t i = 0U; i < m_marksCount; i++) {
             sum += m_marks[i];
         }
 
-        return static_cast<double>(sum) / 7.0;
+        return static_cast<double>(sum) / m_marksCount;
     }
     char *C_Student::getName() { return m_name; }
 
     char *C_Student::getSurname() { return m_surname; }
 
-    uint16_t *C_Student::getMarks() { return &m_marks[0]; }
+    uint16_t *C_Student::getMarks() { return m_marks; }
 
-    uint16_t C_Student::getGroup() { return m_group; }
+    uint16_t C_Student::getMarksCount() { return m_marksCount; }
 } // namespace cls
